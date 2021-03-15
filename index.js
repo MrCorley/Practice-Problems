115d14731622e77ccff5af94a3c38af91211ee35
Jonathan Corley <jcorley@noscihigh.org>
2021-03-15 15:52:00 +0000

    Updated assignment with more problems

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+
+///Strings
+//First, declare variable named firstName.
+
+//Assign firstName to a string value of your first name.
+
+//Declare another variable named lastName and assign it the value of your last name.
+
+//Then declare another variable named fullName and assign it to the current value of firstName and lastName and have the joined values separated with a space character.
+
+//Remember that computer languages will not add the space character for you so how would you do that manually?
+
+
+
+
+
+
+
+
+//Numbers
+//Declare a variable named luckyNumber.
+
+//Assign luckyNumber to a number of your choice.
+
+//Declare another variable named luckyNumberTimes2 and assign it the value of luckyNumber multiplied by 2.
+
+
+
+
+
+
+
+
+
+
+
+//Booleans
+//Declare a variable named myBool and assign it the value of true.
+
+
+//Then, declare another named isEqualTo and assign it the value of strictly comparing luckyNumber and luckyNumberTimes2.
+
+
+
+
+
 /* 
 1. Create a function factorial that when given a 
 number, returns the factorial of it. The factorial
@@ -47,4 +93,22 @@ function reverseString(string){
 
 
   //CODE ABOVE HERE
+}
+
+
+
+
+
+/*
+3. Create a function that takes an input array and 
+add all numbers in the array.
+
+Example: 
+var myArray = [1,2,3,4,5]
+
+arraySum(myArray) => 15
+*/
+function arraySum(array){
+   
+    
 }
